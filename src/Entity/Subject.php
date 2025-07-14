aba87d4822b595730472f92c73ccc3aed43292bc
quueli <rampadian@mail.ru>
2025-07-15 01:37:07 +0300

    subject synonyms

--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -17,17 +17,24 @@ class Subject
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'subject_name_required')]
+    #[Assert\Length(max: 255, maxMessage: 'subject_name_too_long')]
     private ?string $name = null;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'subjects')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
 
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $synonym = null;
+
     #[ORM\Column(type: 'integer', nullable: true)]
     private ?int $sortOrder = null;
 
     #[ORM\Column(type: 'datetime')]
     private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
 
     public function __construct()
     {
@@ -61,6 +68,17 @@ class Subject
         return $this;
     }
 
+    public function getSynonym(): ?string
+    {
+        return $this->synonym;
+    }
+
+    public function setSynonym(?string $synonym): static
+    {
+        $this->synonym = $synonym;
+        return $this;
+    }
+
     public function getSortOrder(): ?int
     {
         return $this->sortOrder;
@@ -81,6 +99,28 @@ class Subject
     {
         $this->createdAt = $createdAt;
         return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function getCategoryPath(): string
+    {
+        return $this->category ? $this->category->getPathString() : '';
+    }
+
+    public function getFullName(): string
+    {
+        $path = $this->getCategoryPath();
+        return $path ? $path . ' / ' . $this->name : $this->name;
     }
 
     public function __toString(): string
