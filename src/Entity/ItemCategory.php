3965327b0ca27e98143d64a44795ff82d60e7371
quueli <rampadian@mail.ru>
2025-08-14 12:05:31 +0300

    label and path for the editor

--- src/Entity/ItemCategory.php
+++ src/Entity/ItemCategory.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ItemCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -24,6 +26,7 @@ class ItemCategory
 
     #[ORM\Column(type: 'string', length: 50)]
     #[Assert\NotBlank(message: 'segment_required')]
+    #[Assert\Choice(choices: ['components', 'modules', 'fasteners', 'enclosures', 'accessories'], message: 'invalid_segment')]
     private ?string $segment = null;
 
     #[ORM\ManyToOne(targetEntity: Category::class)]
@@ -34,6 +37,12 @@ class ItemCategory
     #[ORM\JoinColumn(nullable: true)]
     private ?Subject $subject = null;
 
+    #[ORM\OneToMany(mappedBy: 'itemCategory', targetEntity: ItemCategoryAttributeValue::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
+    private Collection $attributeValues;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $sortOrder = null;
+
     #[ORM\Column(type: 'datetime')]
     private ?\DateTimeInterface $createdAt = null;
 
@@ -42,6 +51,7 @@ class ItemCategory
 
     public function __construct()
     {
+        $this->attributeValues = new ArrayCollection();
         $this->createdAt = new \DateTime();
         $this->updatedAt = new \DateTime();
     }
@@ -95,6 +105,32 @@ class ItemCategory
         return $this;
     }
 
+    public function getAttributeValues(): Collection
+    {
+        return $this->attributeValues;
+    }
+
+    public function addAttributeValue(ItemCategoryAttributeValue $attributeValue): static
+    {
+        if (!$this->attributeValues->contains($attributeValue)) {
+            $this->attributeValues->add($attributeValue);
+            $attributeValue->setItemCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAttributeValue(ItemCategoryAttributeValue $attributeValue): static
+    {
+        if ($this->attributeValues->removeElement($attributeValue)) {
+            if ($attributeValue->getItemCategory() === $this) {
+                $attributeValue->setItemCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
@@ -121,5 +157,44 @@ class ItemCategory
     public function setUpdatedAtValue(): void
     {
         $this->updatedAt = new \DateTime();
+    }
+
+    public function getLabel(): string
+    {
+        if ($this->subject) {
+            return $this->subject->getName();
+        }
+
+        if ($this->category) {
+            return $this->category->getName();
+        }
+
+        return 'unknown';
+    }
+
+    public function getPath(): string
+    {
+        if ($this->subject) {
+            return $this->subject->getCategory()
+                ? $this->subject->getCategory()->getPathString() . ' / ' . $this->subject->getName()
+                : $this->subject->getName();
+        }
+
+        if ($this->category) {
+            return $this->category->getPathString();
+        }
+
+        return 'unknown';
+    }
+
+    public function getSortOrder(): ?int
+    {
+        return $this->sortOrder;
+    }
+
+    public function setSortOrder(?int $sortOrder): static
+    {
+        $this->sortOrder = $sortOrder;
+        return $this;
     }
 }
